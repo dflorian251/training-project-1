f518f6703dd0128e783045eb6010d3209bc44982
dflorian251 <inf2021044@ionio.gr>
2024-07-15 18:41:00 +0300

    UI of the event form updated.

--- resources/js/Layouts/Form.vue
+++ resources/js/Layouts/Form.vue
@@ -7,7 +7,7 @@ import axios from 'axios';
 import {createRouter} from 'vue-router';
 
 
-const formData = ref({
+const form = ref({
   name: '',
   publicity: '',
   date: '',
@@ -16,7 +16,7 @@ const formData = ref({
 
 const submitForm = async () => {
   try {
-    const response = await axios.post('/training-project-1/public/submit-form', formData.value).then(function (response) {
+    const response = await axios.post('/training-project-1/public/submit-form', form.value).then(function (response) {
         window.location = response.data.redirect;
     });
     console.log('Data submitted successfully:', response.data);
@@ -41,30 +41,26 @@ const submitForm = async () => {
 
         <div>
         <label for="name" class="block text-gray-700">Event Name</label>
-        <input type="text" id="name" name="name" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="formData.name">
+        <input type="text" id="name" name="name" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="form.name">
         </div>
-
 
         <div>
-        <InputLabel for="publicity" class="font-light text-gray-500" value="Public" />
-        <Checkbox
-            id="publicity_checkbox"
-            class="mt-1 block w-full"
-            name="publicity_checkbox"
-            required
-            v-model="formData.publicity"
-        />
+            <InputLabel value="Visibility(?)" />
+            <select id="" v-model="form.publicity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 white:bg-gray-700 white:border-gray-600 dark:placeholder-gray-400 white:text dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected>The event was...</option>
+                <option value="public">Public</option>
+                <option value="private">Private (not public)</option>
+            </select>
         </div>
-
 
         <div>
         <label for="date" class="block text-gray-700">Event Date</label>
-        <input type="date" id="date" name="date" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="formData.date">
+        <input type="date" id="date" name="date" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="form.date">
         </div>
 
         <div>
         <label for="people_attended" class="block text-gray-700">People Attended</label>
-        <input type="number" id="people_attended" name="people_attended" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="formData.people_attended">
+        <input type="number" id="people_attended" name="people_attended" required class="w-full p-2 border border-gray-300 rounded mt-1" v-model="form.people_attended">
         </div>
 
         <PrimaryButton class="ms-4">
